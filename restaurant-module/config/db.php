4a2dd3a42168db8f99b20f8ea9dd678c8513b438
Gino Terlouw <Gino Terlouw>
2016-05-13 14:24:48 +0200

    refined db connection method

--- restaurant-module/config/db.php
+++ restaurant-module/config/db.php
@@ -1,25 +1,42 @@
 <?php
 
-    session_start();
 
-    $user = 'root';
-    $pass = '';
-    $dbname = 'Bontekoe_ALA';
-    $host = '127.0.0.1';
-    $debug = false;
+class DB
+{
+    private static $_instance = null;
 
-    try{
 
-        $conn = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
+    private
+        $_PDO;
 
-        if($debug){
-            if($conn){
-                echo "Success";
+
+    private function __construct()
+    {
+        $_user = 'root';
+        $_pass = '';
+        $_dbname = 'Bontekoe_ALA';
+        $_host = '127.0.0.1';
+        $debug = false;
+
+        try {
+
+            $this->_PDO = new PDO("mysql:host=$_host;dbname=$_dbname", $_user, $_pass);
+            if($debug){
+                echo "ok";
+            }
+
+
+        } catch (PDOException $e) {
+            if($debug){
+                echo "not ok";
             }
         }
-
-    }catch(PDOException $e){
-        if($debug){
-            echo $e->getMessage();
-        }
     }
+
+    public static function getInstance(){
+        if(!isset(self::$_instance)){
+            self::$_instance = new DB();
+        }
+        return self::$_instance;
+    }
+}
---
+++ restaurant-module/test.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Gino0
+ * Date: 5/13/2016
+ * Time: 1:57 PM
+ */
+
+
+include ("config/db.php");
+
+DB::getInstance();
+?>
+
